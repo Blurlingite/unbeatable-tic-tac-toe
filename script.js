f0995c68d1abe9b7daca75503db724b0cc5e0f5e
KingBliz <147171797+KingBliz@users.noreply.github.com>
2024-04-29 20:44:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Initalize the Board
-var origBoard;
+var Board;
 
 //Array of Winning Combos from Left to Right
-const winCombos = [
+const WinningCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
 	[6, 7, 8],
@@ -12,23 +12,23 @@ const winCombos = [
 	[0, 4, 8],
 	[6, 4, 2]
 ]
-//Stores a Reference to the Cells
-const cells = document.querySelectorAll('.cell');
+//Stores a Reference to the Boxes
+const Boxes = document.querySelectorAll('.cell');
 //Function to start the game, runs at the start and when replay is clicked
 startGame();
 
 //Function for selecting variable
-function selectSym(sym){
-    huPlayer = sym;
-    aiPlayer = sym==='O' ? 'X' :'O';
-    origBoard = Array.from(Array(9).keys());
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].addEventListener('click', turnClick, false);
+function SelectionPrompt(SystemSelect){
+    huPlayer = SystemSelect;
+    aiPlayer = SystemSelect==='O' ? 'X' :'O';
+    Board = Array.from(Array(9).keys());
+    for (let i = 0; i < Boxes.length; i++) {
+      Boxes[i].addEventListener('click', TurnBehavior, false);
     }
     if (aiPlayer === 'X') {
-      turn(bestSpot(),aiPlayer);
+      TurnUpdate(bestSpot(),aiPlayer);
     }
-    document.querySelector('.selectSym').style.display = "none";
+    document.querySelector('.SelectionPrompt').style.display = "none";
   }
 
 
@@ -37,60 +37,60 @@ function startGame() {
 //Whenever button is clicked or initalized it is setting values
     document.querySelector(".endgame").style.display = "none";
     document.querySelector('.endgame .text').innerText ="";
-    document.querySelector('.selectSym').style.display = "block";
-    origBoard = Array.from(Array(9).keys());
+    document.querySelector('.SelectionPrompt').style.display = "block";
+    Board = Array.from(Array(9).keys());
     //Clears the board
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].innerText = '';
-        cells[i].style.removeProperty('background-color');
+    for (let i = 0; i < Boxes.length; i++) {
+        Boxes[i].innerText = '';
+        Boxes[i].style.removeProperty('background-color');
 	}
 }
 //Logs the Place an entity clicks
-function turnClick(square) {
-	if (typeof origBoard[square.target.id] ==='number') {
-        turn(square.target.id, huPlayer);
-        //After user takes a turn, AI takes turn for best spot
-        if (!checkWin(origBoard, huPlayer) && !checkTie())  
-          turn(bestSpot(), aiPlayer);
+function TurnBehavior(square) {
+	if (typeof Board[square.target.id] ==='number') {
+        TurnUpdate(square.target.id, huPlayer);
+        //After user takes a TurnUpdate, AI takes TurnUpdate for best spot
+        if (!CheckWinCondition(Board, huPlayer) && !CheckTieCondition())  
+          TurnUpdate(bestSpot(), aiPlayer);
       }
 }
 //Updates the place clicked on the board with the variable we set for Player
-function turn(squareId, player) {
-    origBoard[squareId] = player;
+function TurnUpdate(squareId, player) {
+    Board[squareId] = player;
     document.getElementById(squareId).innerHTML = player;
-    let gameWon = checkWin(origBoard, player);
+    let GameWinCondition = CheckWinCondition(Board, player);
     //Runs gamewon function Then Gameover, Finds who won then runs commands at end of game
-    if (gameWon) gameOver(gameWon);
-    checkTie();
+    if (GameWinCondition) GameOverCondition(GameWinCondition);
+    CheckTieCondition();
   }
   //See if we win
-  function checkWin(board, player) {
+  function CheckWinCondition(board, player) {
     //Finds all places on board that has been played in
     //Reduces all elements to find the places where nothing is played in
     let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
-    let gameWon = null;
+    let GameWinCondition = null;
     //Time to Check if game is won, 
-    for (let [index, win] of winCombos.entries()) {
+    for (let [index, win] of WinningCombos.entries()) {
         //Goes through every Element in array for wincondition
       if (win.every(elem => plays.indexOf(elem) > -1)) {
-        gameWon = {index: index, player: player};
+        GameWinCondition = {index: index, player: player};
         break;
       }
     }
-    return gameWon;
+    return GameWinCondition;
   }
-  //Runs Commands from GameOver after calling GameWon, Ending Displays and Functions of the program, Essentially what happens when game is over
-  function gameOver(gameWon){
+  //Runs Commands from GameOverCondition after calling GameWon, Ending Displays and Functions of the program, Essentially what happens when game is over
+  function GameOverCondition(GameWinCondition){
     //Highlights boxes where entity won, background color depends on who won
-    for (let index of winCombos[gameWon.index]) {
+    for (let index of WinningCombos[GameWinCondition.index]) {
       document.getElementById(index).style.backgroundColor = 
-        gameWon.player === huPlayer ? "blue" : "red";
+        GameWinCondition.player === huPlayer ? "blue" : "red";
     }
     //Makes it so that you cant Click anymore on the board and just displays win or lose
-    for (let i=0; i < cells.length; i++) {
-      cells[i].removeEventListener('click', turnClick, false);
+    for (let i=0; i < Boxes.length; i++) {
+      Boxes[i].removeEventListener('click', TurnBehavior, false);
     }
-    declareWinner(gameWon.player === huPlayer ? "You win!" : "You lose");
+    declareWinner(GameWinCondition.player === huPlayer ? "You win!" : "You lose");
   }
   //Winner
   function declareWinner(who) {
@@ -100,19 +100,19 @@ function turn(squareId, player) {
   }
   //Finds all empty Squares by filtering out every box with an element
   function emptySquares() {
-    return origBoard.filter((elm, i) => i===elm);
+    return Board.filter((elm, i) => i===elm);
   }
     //Finds best spot for Ai using minmax algor
   function bestSpot(){
-    return minimax(origBoard, aiPlayer).index;
+    return minimax(Board, aiPlayer).index;
   }
     //Finds if its a Tie
-  function checkTie() {
+  function CheckTieCondition() {
     if (emptySquares().length === 0){
-      for (cell of cells) {
+      for (cell of Boxes) {
         //Removes the ability to click the board and change color to green
         cell.style.backgroundColor = "green";
-        cell.removeEventListener('click',turnClick, false);
+        cell.removeEventListener('click',TurnBehavior, false);
       }
       declareWinner("Tie game");
       return true;
@@ -123,9 +123,9 @@ function turn(squareId, player) {
   function minimax(newBoard, player) {
     var availSpots = emptySquares(newBoard);
     // Sets values for win cases either player or AI
-    if (checkWin(newBoard, huPlayer)) {
+    if (CheckWinCondition(newBoard, huPlayer)) {
       return {score: -10};
-    } else if (checkWin(newBoard, aiPlayer)) {
+    } else if (CheckWinCondition(newBoard, aiPlayer)) {
       return {score: 10};
     } else if (availSpots.length === 0) {
       return {score: 0};
